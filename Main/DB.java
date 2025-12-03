744c50afaecb3e57fe89e2ccb278ae69cfd5a962
Christian De Leon <145353689+lChristl@users.noreply.github.com>
2025-12-03 20:53:43 +0800

    Create DB.java

---
+++ Main/DB.java
@@ -0,0 +1,19 @@
+package mongoJava;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+public class DB {
+
+	private static MongoClient client = null;
+
+	public static MongoDatabase getDatabase() {
+
+		if (client == null) {
+			client = new MongoClient("localhost", 27017);
+			System.out.println("Connected successfully!");
+		}
+
+		return client.getDatabase("LibManage");
+	}
+}
