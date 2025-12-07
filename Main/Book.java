c8bc2888408394b562a9b3658df9a58db89cbeec
kil <liansandiego141@gmail.com>
2025-12-08 00:17:49 +0800

    Update Book.java

--- Main/Book.java
+++ Main/Book.java
@@ -11,6 +11,7 @@ import java.awt.Frame;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -27,12 +28,22 @@ import com.mongodb.client.MongoCursor;
 import javax.swing.ImageIcon;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.Window.Type;
+import javax.swing.JDesktopPane;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Toolkit;
 
 public class Book {
 
 	JFrame frmLibmanage;
 	private JTable table;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
 
 	/**
 	 * Launch the application.
@@ -64,6 +75,7 @@ public class Book {
 	 */
 	private void initialize() {
 		frmLibmanage = new JFrame();
+		frmLibmanage.setIconImage(Toolkit.getDefaultToolkit().getImage(Book.class.getResource("/mongoJava/photos/Libris_logo (2) (3).png")));
 		frmLibmanage.getContentPane().setBackground(new Color(255, 255, 255));
 		frmLibmanage.setTitle("LibManage");
 		frmLibmanage.setModalExclusionType(ModalExclusionType.APPLICATION_EXCLUDE);
@@ -71,7 +83,6 @@ public class Book {
 		frmLibmanage.setResizable(false);
 		frmLibmanage.setPreferredSize(new Dimension(1920, 1080));
 		frmLibmanage.setAutoRequestFocus(false);
-		frmLibmanage.setAlwaysOnTop(true);
 		frmLibmanage.setBounds(0, 0, 1940, 1080);
 		frmLibmanage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmLibmanage.getContentPane().setLayout(null);
@@ -83,6 +94,188 @@ public class Book {
 		panel.setAlignmentX(Component.RIGHT_ALIGNMENT);
 		frmLibmanage.getContentPane().add(panel);
 		panel.setLayout(null);
+		
+		JDesktopPane desktopPane_1 = new JDesktopPane();
+		desktopPane_1.setBorder(new MatteBorder(5, 5, 5, 5, (Color) new Color(103, 0, 0)));
+		desktopPane_1.setBackground(new Color(255, 255, 255));
+		desktopPane_1.setBounds(367, 282, 452, 150);
+		desktopPane_1.setVisible(false);
+		frmLibmanage.getContentPane().add(desktopPane_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("ID :");
+		lblNewLabel_1_2.setForeground(new Color(28, 28, 28));
+		lblNewLabel_1_2.setFont(new Font("Montserrat Black", Font.PLAIN, 23));
+		lblNewLabel_1_2.setBounds(39, 57, 70, 25);
+		desktopPane_1.add(lblNewLabel_1_2);
+		
+		JLabel lblNewLabel_4_2 = new JLabel("\r");
+		lblNewLabel_4_2.setIcon(new ImageIcon(Dashboard.class.getResource("/mongoJava/photos/Libris_logo (2) (3).png")));
+		lblNewLabel_4_2.setHorizontalTextPosition(SwingConstants.CENTER);
+		lblNewLabel_4_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4_2.setForeground(new Color(28, 28, 28));
+		lblNewLabel_4_2.setBounds(22, 37, 101, 97);
+		panel.add(lblNewLabel_4_2);
+		
+		textField_5 = new JTextField();
+		textField_5.setFont(new Font("Montserrat SemiBold", Font.PLAIN, 15));
+		textField_5.setColumns(10);
+		textField_5.setBounds(111, 55, 286, 34);
+		desktopPane_1.add(textField_5);
+		
+		JLabel lblDeleteBookForm = new JLabel("Delete Book Form");
+		lblDeleteBookForm.setFont(new Font("Monospaced", Font.BOLD, 30));
+		lblDeleteBookForm.setBounds(10, 11, 411, 34);
+		desktopPane_1.add(lblDeleteBookForm);
+		
+		JButton btnDone_1 = new JButton("Done");
+		btnDone_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String id = textField_5.getText();
+				
+				int issue = BookDB.delete(id);
+
+		        if (issue > 0) {
+		        	JOptionPane.showMessageDialog(null, "Book Deleted Successfully!");
+		            Book.main(new String[]{});
+		            frmLibmanage.dispose();
+		        } else {
+		            JOptionPane.showMessageDialog(null, "Sorry, unable to delete!");
+		        }
+
+			}
+		});
+		btnDone_1.setFont(new Font("Montserrat Black", Font.PLAIN, 18));
+		btnDone_1.setBorder(new MatteBorder(3, 3, 3, 3, (Color) new Color(103, 0, 0)));
+		btnDone_1.setBackground(Color.WHITE);
+		btnDone_1.setBounds(353, 100, 89, 34);
+		desktopPane_1.add(btnDone_1);
+		
+		JButton btnNewButton_11_1 = new JButton("x");
+		btnNewButton_11_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				desktopPane_1.setVisible(false);
+			}
+		});
+		btnNewButton_11_1.setFont(new Font("Montserrat Black", Font.PLAIN, 11));
+		btnNewButton_11_1.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnNewButton_11_1.setBackground(Color.LIGHT_GRAY);
+		btnNewButton_11_1.setBounds(403, 11, 39, 23);
+		desktopPane_1.add(btnNewButton_11_1);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane.setBorder(new MatteBorder(5, 5, 5, 5, (Color) new Color(103, 0, 0)));
+		desktopPane.setBackground(new Color(255, 255, 255));
+		desktopPane.setBounds(172, 285, 693, 370);
+		desktopPane.setVisible(false);
+		frmLibmanage.getContentPane().add(desktopPane);
+		
+		JLabel lblNewLabel_1 = new JLabel("ID :");
+		lblNewLabel_1.setFont(new Font("Montserrat Black", Font.PLAIN, 23));
+		lblNewLabel_1.setForeground(new Color(28, 28, 28));
+		lblNewLabel_1.setBounds(127, 77, 70, 25);
+		desktopPane.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Montserrat SemiBold", Font.PLAIN, 15));
+		textField.setBounds(188, 72, 462, 34);
+		desktopPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Title :");
+		lblNewLabel_1_1.setForeground(new Color(28, 28, 28));
+		lblNewLabel_1_1.setFont(new Font("Montserrat Black", Font.PLAIN, 23));
+		lblNewLabel_1_1.setBounds(97, 123, 122, 25);
+		desktopPane.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_1_1_1 = new JLabel("Author :");
+		lblNewLabel_1_1_1.setForeground(new Color(28, 28, 28));
+		lblNewLabel_1_1_1.setFont(new Font("Montserrat Black", Font.PLAIN, 23));
+		lblNewLabel_1_1_1.setBounds(65, 167, 122, 25);
+		desktopPane.add(lblNewLabel_1_1_1);
+		
+		JLabel lblNewLabel_1_1_1_1 = new JLabel("Year :");
+		lblNewLabel_1_1_1_1.setForeground(new Color(28, 28, 28));
+		lblNewLabel_1_1_1_1.setFont(new Font("Montserrat Black", Font.PLAIN, 23));
+		lblNewLabel_1_1_1_1.setBounds(97, 213, 122, 25);
+		desktopPane.add(lblNewLabel_1_1_1_1);
+		
+		JLabel lblNewLabel_1_1_1_1_1 = new JLabel("Available :");
+		lblNewLabel_1_1_1_1_1.setForeground(new Color(28, 28, 28));
+		lblNewLabel_1_1_1_1_1.setFont(new Font("Montserrat Black", Font.PLAIN, 23));
+		lblNewLabel_1_1_1_1_1.setBounds(33, 262, 164, 25);
+		desktopPane.add(lblNewLabel_1_1_1_1_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setFont(new Font("Montserrat SemiBold", Font.PLAIN, 15));
+		textField_1.setColumns(10);
+		textField_1.setBounds(188, 119, 462, 34);
+		desktopPane.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setFont(new Font("Montserrat SemiBold", Font.PLAIN, 15));
+		textField_2.setColumns(10);
+		textField_2.setBounds(188, 162, 462, 34);
+		desktopPane.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setFont(new Font("Montserrat SemiBold", Font.PLAIN, 15));
+		textField_3.setColumns(10);
+		textField_3.setBounds(188, 208, 462, 34);
+		desktopPane.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setFont(new Font("Montserrat SemiBold", Font.PLAIN, 15));
+		textField_4.setColumns(10);
+		textField_4.setBounds(188, 257, 462, 34);
+		desktopPane.add(textField_4);
+		
+		JLabel lblAddBook = new JLabel("Add Book Form");
+		lblAddBook.setFont(new Font("Monospaced", Font.BOLD, 32));
+		lblAddBook.setBounds(23, 11, 411, 50);
+		desktopPane.add(lblAddBook);
+		
+		JButton btnDone = new JButton("Done");
+		btnDone.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				 	String id = textField.getText();
+			        String title = textField_1.getText();
+			        String author = textField_2.getText();
+			        int year = Integer.parseInt(textField_3.getText());
+			        int avail = Integer.parseInt(textField_4.getText());
+			        int transact = 0;
+			   
+			        int issue = BookDB.save(id, title, author, year, avail, transact);
+
+			        if (issue > 0) {
+			        	JOptionPane.showMessageDialog(null, "Book Added Successfully!");
+			            Book.main(new String[]{});
+			            frmLibmanage.dispose();
+			        } else {
+			            JOptionPane.showMessageDialog(null, "Sorry, unable to add!");
+			        }
+
+			    }
+			});
+			
+	
+		btnDone.setFont(new Font("Montserrat Black", Font.PLAIN, 18));
+		btnDone.setBorder(new MatteBorder(3, 3, 3, 3, (Color) new Color(103, 0, 0)));
+		btnDone.setBackground(Color.WHITE);
+		btnDone.setBounds(561, 312, 89, 34);
+		desktopPane.add(btnDone);
+		
+		JButton btnNewButton_11 = new JButton("x");
+		btnNewButton_11.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				desktopPane.setVisible(false);
+			}
+		});
+		btnNewButton_11.setFont(new Font("Montserrat Black", Font.PLAIN, 11));
+		btnNewButton_11.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnNewButton_11.setBackground(Color.LIGHT_GRAY);
+		btnNewButton_11.setBounds(644, 11, 39, 23);
+		desktopPane.add(btnNewButton_11);
+		
 		
 		JLabel lblNewLabel_3 = new JLabel("Home");
 		lblNewLabel_3.setForeground(new Color(28, 28, 28));
@@ -288,6 +481,11 @@ public class Book {
 		frmLibmanage.getContentPane().add(scrollPane);
 		
 		JButton btnAddBook = new JButton("+ Add Book");
+		btnAddBook.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				desktopPane.setVisible(true);
+			}
+		});
 		btnAddBook.setForeground(new Color(28, 28, 28));
 		btnAddBook.setFont(new Font("Montserrat Black", Font.PLAIN, 20));
 		btnAddBook.setBorder(new MatteBorder(3, 3, 3, 3, (Color) new Color(103, 0, 0)));
@@ -295,7 +493,13 @@ public class Book {
 		btnAddBook.setBounds(170, 223, 166, 51);
 		frmLibmanage.getContentPane().add(btnAddBook);
 		
+		
 		JButton btnDeleteBook = new JButton("Delete Book");
+		btnDeleteBook.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				desktopPane_1.setVisible(true);
+			}
+		});
 		btnDeleteBook.setForeground(new Color(28, 28, 28));
 		btnDeleteBook.setFont(new Font("Montserrat Black", Font.PLAIN, 20));
 		btnDeleteBook.setBorder(new MatteBorder(3, 3, 3, 3, (Color) new Color(103, 0, 0)));
